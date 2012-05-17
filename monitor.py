5d90d28da12332a957d9981217e32f3250d2f70c
Alex Luke <alexl@bnj.com>
2012-05-17 16:36:17 -0700

    Rendered status template

--- monitor.py
+++ monitor.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask import render_template
 import boto
 app = Flask(__name__)
 app.config.from_pyfile('settings.cfg')
 
+@app.template_filter()
+def yesno(value, yes, no):
+	if value:
+		return yes
+	return no
+
 @app.route('/')
-def lbs():
+def elb_status():
 	conn = boto.connect_elb(
 		aws_access_key_id=app.config['ACCESS_KEY'],
 		aws_secret_access_key=app.config['SECRET_KEY']
@@ -12,18 +19,22 @@ def lbs():
 	load_balancers = conn.get_all_load_balancers(app.config['LOAD_BALANCERS'])
 	lbs = []
 	for lb in load_balancers:
-		instances = lb.get_instance_health()
+		instances = []
+		for inst in lb.get_instance_health():
+			instances.append({
+				'id': inst.instance_id,
+				'up': inst.state == 'InService',
+			})
 		lb_status = {
 			'name': lb.name,
 			'instances': instances,
-			'status': 'ok',
+			'redundant': True,
 		}
-		if len(instances) < 2 or not all([i.state == 'InService' for i in instances]):
-			lb_status['status'] = 'reduced'
+		if len(instances) < 2 or not all([i['up'] for i in instances]):
+			lb_status['redundant'] = False
 		
 		lbs.append(lb_status)
-	import pprint
-	return pprint.pformat(lbs)
+	return render_template('elb_status.html', lbs=lbs)
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
